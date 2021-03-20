94cf525d4b55e9009c84a0460a68006e6020efe1
Colin Morrison <colinjmmorris@gmail.com>
2021-03-20 21:33:29 +0000

    Extensions complete

--- Week_02/Day_5/karaoke/src/room.py
+++ Week_02/Day_5/karaoke/src/room.py
@@ -1,7 +1,7 @@
 class Room:
     def __init__(self, room_number, song_list):
         self.room_number = room_number
-        self.song_list = song_list #We have an initial song list assigned to the room. This is a dictionary
+        self.song_list = song_list #We have an initial song list assigned to the room
         
         self.guest_list = [] #There are no guests initially
         self.room_capacity = 4 # The room capcity of all rooms is 4
--- Week_02/Day_5/karaoke/src/song.py
+++ Week_02/Day_5/karaoke/src/song.py
@@ -2,4 +2,3 @@ class Song:
     def __init__(self, title, artist):
         self.title = title
         self.artist = artist
-        # self.song_info = {title : artist} # Assigns the two arguments as a key value pair
